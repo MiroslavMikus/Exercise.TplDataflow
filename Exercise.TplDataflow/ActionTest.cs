dad0f55d4aa69fa2cbae691d70855cf949d1212c
Mikus Miroslav <miroslav.mikus@outlook.com>
2019-01-23 06:52:13 +0100

    Add ActionBlock example

--- Exercise.TplDataflow/ActionTest.cs
+++ Exercise.TplDataflow/ActionTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Exercise.TplDataflow
@@ -7,8 +9,45 @@ namespace Exercise.TplDataflow
     public class ActionTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task SimpleAction()
         {
+            var block = new ActionBlock<int>(n =>
+            {
+                Console.WriteLine(n);
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                block.Post(i);
+            }
+
+            block.Complete();
+
+            Console.WriteLine("Done!");
+
+            await block.Completion;
+        }
+
+        [TestMethod]
+        public async Task ActionWithDelay()
+        {
+            var block = new ActionBlock<int>(n =>
+            {
+                Task.Delay(500).Wait();
+                Console.WriteLine(n);
+            });
+
+            for (int i = 0; i < 10; i++)
+            {
+                block.Post(i);
+                Console.WriteLine($"Action queue: {block.InputCount}");
+            }
+
+            block.Complete();
+
+            Console.WriteLine("Done!");
+
+            await block.Completion;
         }
     }
 }
